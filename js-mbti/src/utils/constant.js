245bacb300b5dedcc01c152b7c847a68a9753b42
1wooseok <74036731+1wooseok@users.noreply.github.com>
2022-08-22 17:40:36 +0900

    스토어 만들어보기

--- js-mbti/src/utils/constant.js
+++
@@ -1,7 +0,0 @@
-const MAX_QUIZ_LENGTH = 12;
-
-const ANIMAL_TYPES = ["mouse", "cow", "tiger", "rabbit", "dragon", "snake", "horse", "sheep", "monkey", "chick", "dog", "pig"]
-
-export default MAX_QUIZ_LENGTH;
-
-export { ANIMAL_TYPES }
